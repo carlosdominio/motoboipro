0f76a805db79e2dadea9a8589bddc3a024922aea
carlosdominio <chfla@live.com>
2026-05-27 14:11:12 -0300

    build: limpeza de ambiente, correções de produção e preparação para deploy 2026

--- test_end.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const buffer = fs.readFileSync('garconnexpress/frontend/admin/index.html');
-const lastPart = buffer.slice(-100);
-console.log(lastPart.toString('utf8'));
-console.log(lastPart.toString('hex'));
